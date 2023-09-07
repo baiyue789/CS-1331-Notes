d642d04a9a49b110e113c041e75e6b8b144fbb41
baiyue789 <73799464+baiyue789@users.noreply.github.com>
2023-09-07 15:59:09 -0400

    Update Strings_And_IO.java

--- Strings_And_IO.java
+++ Strings_And_IO.java
@@ -8,8 +8,17 @@ public class Strings_And_IO{
 /*Sting are constand they cannot be changed. Once you create a string you cannot change it's character so if you do change it a ghost variable is created*/
 //codePoints: UNknown not taught yet
     public static void main(String [] args){
-        for (int i = 0; i < 5; i++) {
+        String a = "Suzzy";
+        Sting b = "Mark";
+        System.out.println(a);
+        System.out.println(b);
+        
+
+        a=b;
+        System.out.println(a);
+        System.out.println(b);
+        /*for (int i = 0; i < 5; ++i) {
             System.out.println(i);
-        }
+        }*/
     }
 }
