26626a295137cb13ab7602b271031e559e75070b
baiyue789 <73799464+baiyue789@users.noreply.github.com>
2023-09-07 15:53:10 -0400

    Update Strings_And_IO.java

--- Strings_And_IO.java
+++ Strings_And_IO.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
-
+public class Strings_And_IO{
 //Strings are strings of characters
 //matlab it is a vector of characte
 //C++ it is an array of characters
 //Java Strings are objects
 
+/*Sting are constand they cannot be changed. Once you create a string you cannot change it's character so if you do change it a ghost variable is created*/
+//codePoints: UNknown not taught yet
+    public static void main(String [] args){
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
